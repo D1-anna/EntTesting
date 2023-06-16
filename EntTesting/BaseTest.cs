dae982241a034ce8b11c32556702bf29472f8333
D1-anna <diana.pavlovych@globallogic.com>
2023-06-16 21:49:40 +0300

    refactioring#1

--- EntTesting/BaseTest.cs
+++ EntTesting/BaseTest.cs
@@ -1,14 +1,14 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-
+using SeleniumExtras.WaitHelpers;
 namespace EntTesting
 {
     [TestFixture]
     public class BaseTest
     {
         protected IWebDriver drv;
-        
+        protected WebDriverWait wait;
 
         [OneTimeSetUp]
         public void Setup()
@@ -17,61 +17,60 @@ namespace EntTesting
             options.AddArgument("start-maximized");
             drv = new ChromeDriver(options);
             LoginTest();
+
+            new WebDriverWait(drv, TimeSpan.FromSeconds(5));
         }
         public void LoginTest()
         {
-            WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(2));
-
             drv.Navigate().GoToUrl(Environment.GetEnvironmentVariable("ENT_QA_URL"));
             var _invalidCompanyName = "Invalid company name";
             var _invalidNameOrPassword = "Invalid User ID or Password";
 
             //Attempt 1 - enter invalid company name
             var _userName = By.XPath("/html/body/div[1]/div[2]/form/div/div[2]/div[3]/input");
-            drv.FindElement(_userName).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
-            var _password = By.Name("password");
-            drv.FindElement(_password).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS"));
+            drv.FindElement(_userName)
+                .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
+            var _password = By.Id("password");
+            drv.FindElement(_password)
+                .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS"));
             var _companyName = By.Name("_companyText");
             drv.FindElement(_companyName).SendKeys("abcsa");
             drv.FindElement(By.ClassName("login-submit-button")).Click();
+
             var _error = drv.FindElement(By.CssSelector("div.validation-summary-errors li"));
-            Assert.That(_error.Text, Is.EqualTo(_invalidCompanyName));
+            Assert.That(_error.Text, Is.EqualTo(_invalidCompanyName));            
             
+            //Attempt 2 - clear fields and try to enter invalid password  
+            drv.FindElement(_password).SendKeys("eeeeeee");
 
-            //Attempt 2 - clear fields and try to enter invalid password
-            wait.Until(drv =>  drv.FindElement(_password)).Click();
-
-            drv.FindElement(_password);
-
-            wait.Until(drv => drv.FindElement(_companyName)).Click();
-            wait.Until(drv => drv.FindElement(_companyName)).Clear();
-            wait.Until(drv => drv.FindElement(_companyName))
+            drv.FindElement(_companyName).Click();
+            drv.FindElement(_companyName).Clear();
+            drv.FindElement(_companyName)
                 .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
             drv.FindElement(By.ClassName("login-submit-button")).Click();
             
             var _error2 = drv.FindElement(By.CssSelector("div.validation-summary-errors li"));
             Assert.That(_error2.Text, Is.EqualTo(_invalidNameOrPassword));
-
+            
             //Attempt 3 - clear fields and try to enter valid creades
-
-           // wait.Until(drv => drv.FindElement(_userName)).Click();
-            wait.Until(drv => drv.FindElement(_userName)).Clear();
-            wait.Until(drv => drv.FindElement(_password)).Clear();
-            wait.Until(drv => drv.FindElement(_companyName)).Clear();
+            drv.FindElement(_userName).Clear();
+            drv.FindElement(_password).Clear();
+            drv.FindElement(_companyName).Clear();           
            
-           
-            //try valid creads
-            drv.FindElement(_userName).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
-            drv.FindElement(_password).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS"));
-            drv.FindElement(_companyName).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
+            //valid creads
+            drv.FindElement(_userName)
+                .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
+            drv.FindElement(_password)
+                .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS"));
+            drv.FindElement(_companyName)
+                .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
             drv.FindElement(By.ClassName("login-submit-button")).Click();
 
             var _logo = drv.FindElement(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop"));
             Assert.IsTrue(_logo.Displayed);
-            var title = wait.Until(drv => drv.FindElement(By.CssSelector("div.page-title")));
+            var title = drv.FindElement(By.CssSelector("div.page-title"));
             Assert.That(title.Text, Is.EqualTo("MY DASHBOARD"));
-
-            Thread.Sleep(4000);
+            Thread.Sleep(5000);
         }
 
         [OneTimeTearDown]
--- EntTesting/WO.cs
+++ EntTesting/WO.cs
@@ -12,38 +12,34 @@ namespace EntTesting
     [TestFixture]
     public class WO: BaseTest
     {
-        //WebDriverWait wait;
-
         [OneTimeSetUp]
         public void Init()
         {
-            var basic = new BaseTest();
-           // basic.LoginTest();            
+            var basic = new BaseTest();           
         }
        
-
         [Test]
         public void FindWOListPage()
         {
-
-            WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(4));
-            
-            var _woInNavBar = wait.Until(drv => drv.FindElement(By.XPath("//div[contains(@class, 'menu-primary')]/ul[contains(@class, 'menu-list')]//a[text()='Work Orders']")));
+             drv.FindElement(By.CssSelector("nav div.menu-primary ul.menu-list"));
+            IWebElement _woInNavBar = drv.FindElement(By.CssSelector("nav div.menu-primary ul.menu-list li:nth-child(2) a"));
             _woInNavBar.Click();
-            
+            //wait.Until(drv => drv.FindElement(By.CssSelector("nav div.menu-primary ul.menu-list li:nth-child(2) a"))).Click();            
             var _woMenu = drv.FindElement(By.LinkText("List"));
             _woMenu.Click();            
-
             var _titleOfListPage = drv.FindElement(By.ClassName("page-title")).Text;
             Assert.That(_titleOfListPage, Is.EqualTo("WORK ORDERS"));
 
-            var _number = FindWO();          
-            
-
+            Thread.Sleep(4000);
+            var _number = FindWO();
+            Console.WriteLine($"Number #: {_number}");
+            //Thread.Sleep(4000);
             WOQV();
-            wait.Until(drv => drv.FindElement(By.CssSelector("table")));
 
-            //var _woSratusCheck = drv.FindElement(By.CssSelector("table"));
+            //wait.Until(drv => drv.FindElement(By.CssSelector("table")));
+            var woTable = drv.FindElement(By.CssSelector("table"));
+            wait.Until(ExpectedConditions.StalenessOf(woTable));
+
             var woStatus =
             drv.FindElement(By.XPath(
                 $"//td[@data-column='Number']/a[contains(text(), '{_number}')]/../../td[@data-column='WOStatus']"));
@@ -54,33 +50,23 @@ namespace EntTesting
        
         public string FindWO()
         {
-            //var _statusSort = drv.FindElement(By.CssSelector("th[data-filed='WOStatus']"));
-            //_statusSort.Click();
-            /*
-            var woLink = By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a");
-            var woNumber = drv.FindElement(woLink).Text;
-            drv.FindElement(woLink).Click();
-            return woNumber;         
-            */
-            Thread.Sleep(2000);
+            //NextPage();
+
             IWebElement _tableLocation = drv.FindElement(By.XPath("//div[@class='id-wo-list-grid kg-container']"));
             ReadOnlyCollection<IWebElement> rows = _tableLocation.FindElements(By.CssSelector("tbody>tr"));
 
             var newRows = new List<IWebElement>();
-
             var _newSattus = By.TagName("td");
 
             foreach (var item in rows)
             {
-
                 if (item.FindElements(_newSattus)[1].Text == "New")
                 {
                     newRows.Add(item);
-                }
-               
+                }               
             }
 
-            var _number = newRows[0].FindElement(By.CssSelector("td[data-column='Number']"));
+            var _number = newRows[0].FindElement(By.CssSelector("td[data-column='Number'] > a"));
             Console.WriteLine($"Number WO#: {_number.Text}");
             _number.Click();
             return _number.Text;
@@ -89,10 +75,10 @@ namespace EntTesting
 
         public void WOQV()
         {
-
-            WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(2));
-            //var _sectionDisplayed = wait.Until(drv => drv.FindElement(By.CssSelector("div.fpo-section.has-frame div.WoQvSchedulingLabelValueFpoSection")));
+            new WebDriverWait(drv, TimeSpan.FromSeconds(5)).Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div[data-role='woqvdialog']")));
+           
             var _sectionDisplayed = drv.FindElement(By.CssSelector("div.fpo-section.has-frame div.WoQvSchedulingLabelValueFpoSection"));
+            
 
             if (!_sectionDisplayed.Displayed)
             {
@@ -107,7 +93,7 @@ namespace EntTesting
         
         public void CheckAssignee()
         {
-            WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(2));
+            //WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(2));
             var value = drv.FindElement(By.CssSelector("div.id-fpo-section-content.label-value-fpo-section.WoQvSchedulingLabelValueFpoSection div.label-value-rowgroup-wrapper > div > div:nth-child(2) >div:nth-child(3) >div.lv-value"));
 
             if (value.Text == "- -")
@@ -116,15 +102,17 @@ namespace EntTesting
                 var _assignButton = drv.FindElement(By.CssSelector("div.id-fpo-section-header div.id-fpo-section-level-actions ul.plain-actions li"));
                 _assignButton.Click();
 
+                var assing = drv.FindElement(By.CssSelector("div#WoAssignUsersGrid.id-tab-users.kg-container"));
+                wait.Until(ExpectedConditions.StalenessOf(assing));
+
                 var _userTab = wait.Until(drv => drv.FindElement(By.CssSelector("form.corrigo-form div.id-tabbar ul.nav-tabs li a[href='#users']")));
                 new Actions(drv).MoveToElement(_userTab).Click().Perform();
-                //_userTab.Click();
 
+                var userTabContent = "div#IdWoActionAssignEditDialog.BaseAssignmentDialog";
+                wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(userTabContent)));
 
-                wait.Until(drv => drv.FindElement(By.CssSelector("div.tab-content form div.id-filter-w_DisplayAs input"))).SendKeys(_assigneUser);
-
+                drv.FindElement(By.CssSelector("div.tab-content form div.id-filter-w_DisplayAs input")).SendKeys(_assigneUser);
                 wait.Until(drv => drv.FindElement(By.CssSelector("div.tab-content div#users form div.search-buttons button"))).Click();
-
                 var _selectUser = drv.FindElement(By.CssSelector("div#WoAssignUsersGrid tbody[data-role='corrigotooltip'] td.actions-cell"));
                 new Actions(drv).MoveToElement(_selectUser).Perform();
                 var _selectArrow = drv.FindElement(By.CssSelector("div[title='Select']"));
@@ -132,6 +120,8 @@ namespace EntTesting
                 var _savebtn = drv.FindElement(By.CssSelector("div.modal-footer div.right button.id-btn-save"));
                 _savebtn.Click();
 
+                var modalDialog = drv.FindElement(By.CssSelector("div.modal-body"));
+                wait.Until(ExpectedConditions.StalenessOf(modalDialog));
                 PickUpWO();
             }
             else PickUpWO();
@@ -139,13 +129,13 @@ namespace EntTesting
      
         public void PickUpWO()
         {
-            WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(2));
+            
+
             var _comment = "Automation test";
 
             var _PikUpBtn = wait.Until(drv => drv.FindElement(By.CssSelector("form.has-main-fpo-area div.main-action span")));
                 _PikUpBtn.Click();
 
-            //wait.Until(drv => drv.FindElement(By.CssSelector("div[data-role='woactionpickupeditdialog']")));
             var textarea = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//form[@class='corrigo-form']/div/textarea")));            
             new Actions(drv).MoveToElement(textarea).Click().Perform();
             textarea.SendKeys(_comment);
@@ -153,13 +143,22 @@ namespace EntTesting
             var _saveBtnOnPickUpWOdialog = drv.FindElement(By.CssSelector("div[data-role=woactionpickupeditdialog] button.id-btn-save"));
             _saveBtnOnPickUpWOdialog.Click();
 
-            var _actionCol = wait.Until(drv => drv.FindElement(By.CssSelector("td[data-column='ActionTitle']"))).Text;
+            var table = drv.FindElement(By.CssSelector("div[data-role=woactivityloggrid] tbody"));
+            wait.Until(ExpectedConditions.StalenessOf(table));
+
+            var _actionCol = drv.FindElement(By.CssSelector("td[data-column='ActionTitle']")).Text;
             Assert.That(_actionCol, Is.EqualTo("Picked Up"));
-            var _commentCol = wait.Until(drv => drv.FindElement(By.CssSelector("td[data-column='Comment']"))).Text;
+            var _commentCol = drv.FindElement(By.CssSelector("td[data-column='Comment']")).Text;
             Assert.That(_commentCol, Is.EqualTo(_comment));
 
             drv.FindElement(By.CssSelector("div.modal-footer div.right button.id-btn-close")).Click();
 
+        }
+        public void NextPage()
+        {
+            IWebElement nextArrow = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div.k-grid-pager a[title='Go to the next page']")));
+            new Actions(drv).MoveToElement(nextArrow).Click().Perform();
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.TagName("table")));
         }
 
     }
